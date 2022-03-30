871be20b5c4b0aa7f555974292165b984097b25e
giagra91 <giagra91@gmail.com>
2022-03-30 15:04:17 +0200

    inizializzazione esercizio

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="my-header">
+        <img class="m-3" src="../assets/images/logo.svg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndexHeader",
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/partials/variables.scss";
+
+div.my-header{
+    height: 9vh;
+    background-color: $BrandColor;
+
+    img{
+        height: 55%;
+        vertical-align: middle;
+    }
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="my-products">
+        <Card />
+    </div>
+</template>
+
+<script>
+import Card from "./Card.vue"
+
+export default {
+    name: "IndexMain",
+    components:{
+        Card
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/partials/variables.scss";
+    div.my-products{
+        height: 91vh;
+        background-color: $BgColor;
+    }
+
+
+</style>
